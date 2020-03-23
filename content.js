5ddb87c3807ec6c02160793548d759eb9578fe9f
codeconcept <contact@codeconcept.fr>
2020-03-23 19:55:08 +0100

    Translate user selection

--- content.js
+++ content.js
@@ -3,7 +3,32 @@ console.log("Hello Extension");
 function getUserSelection() {
   console.log("up");
   if (window.getSelection) {
-    console.log(window.getSelection().toString());
+    const selection = window.getSelection().toString();
+    console.log("selection", selection);
+
+    const payload = {
+      text: selection.trim(),
+      src: "English",
+      dest: "French",
+      detected: "English",
+      email: "ui@frengly.com",
+      password: "ui123"
+    };
+
+    fetch("https://www.frengly.com/frengly/data/translate/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    })
+      .then(data => data.json())
+      .then(res => {
+        console.log("res", res);
+        if (res.list && res.list.length > 0) {
+          console.log("traduction", res.list[0].destWord);
+        }
+      });
   }
 }
 
