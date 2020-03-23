ac8ada105d42258f0d45ce49fe93096c97aaa436
codeconcept <contact@codeconcept.fr>
2020-03-23 18:49:36 +0100

    Get user selection

--- content.js
+++ content.js
@@ -1,1 +1,10 @@
 console.log("Hello Extension");
+
+function getUserSelection() {
+  console.log("up");
+  if (window.getSelection) {
+    console.log(window.getSelection().toString());
+  }
+}
+
+document.addEventListener("mouseup", getUserSelection);
